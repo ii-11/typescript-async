bd0bbb15f778fbbc144e369657ed6e8fbfd4f714
ii-11 <ivanovski@vivaldi.net>
2020-06-04 13:44:06 +0200

    Callback examples

--- 04-callback/begin/src/examples/get-ingredients.ts
+++ 04-callback/begin/src/examples/get-ingredients.ts
@@ -5,3 +5,13 @@ import { ingredients } from './ingredients';
  * Wait for a delay, get the ingredients,
  * then pass them in a callback function
  */
+
+export function getDataAterDelay(
+  delayMs: number,
+  callback: (data: string[]) => void,
+) {
+  setTimeout(() => {
+    const data = ingredients;
+    callback(data);
+  }, delayMs)
+}
